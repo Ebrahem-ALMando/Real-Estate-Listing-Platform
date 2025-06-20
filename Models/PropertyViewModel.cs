e6486cb5bc94950fd95163f9d5bd0f724f2fe549
Ebrahem Al Mando <ebrahemalmando2001@gmail.com>
2025-06-20 20:27:50 +0300

    upload files

---
+++ Models/PropertyViewModel.cs
@@ -0,0 +1,8 @@
+﻿public class PropertyViewModel
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Price { get; set; }
+    public string Description { get; set; }
+    public List<string> Images { get; set; }
+}
